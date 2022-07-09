9426b395358c99b377240a667ee67a30dba5c378
Rustam <rustam.sb@yandex.ru>
2022-07-10 02:09:29 +0300

    feat: add collection-card section

--- source/js/umnyash.js
+++ source/js/umnyash.js
@@ -1,3 +1,5 @@
+// Popup
+
 const Keys = {
   ESCAPE: 'Escape',
   ESC: 'Esc',
@@ -8,38 +10,96 @@ const isEscEvent = (evt) => {
 };
 
 const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close')
 
-const openPopup = () => {
-  popup.classList.add('popup--open');
-  document.body.classList.add('no-scroll');
-  document.addEventListener('keydown', onPopupEscKeydown);
-};
+if (popup) {
+  const popupCloseButton = popup.querySelector('.popup__close')
 
-const closePopup = () => {
-  popup.classList.remove('popup--open');
-  document.body.classList.remove('no-scroll');
-  document.removeEventListener('keydown', onPopupEscKeydown);
-};
+  const openPopup = () => {
+    popup.classList.add('popup--open');
+    document.body.classList.add('no-scroll');
+    document.addEventListener('keydown', onPopupEscKeydown);
+  };
 
-const onPopupEscKeydown = (evt) => {
-  if (isEscEvent(evt)) {
-    evt.preventDefault();
-    closePopup();
-  }
-};
+  const closePopup = () => {
+    popup.classList.remove('popup--open');
+    document.body.classList.remove('no-scroll');
+    document.removeEventListener('keydown', onPopupEscKeydown);
+  };
 
-popupCloseButton.addEventListener('click', closePopup);
+  const onPopupEscKeydown = (evt) => {
+    if (isEscEvent(evt)) {
+      evt.preventDefault();
+      closePopup();
+    }
+  };
 
-openPopup();
+  popupCloseButton.addEventListener('click', closePopup);
+
+  openPopup();
+}
+
+
+// Number Field
 
 const numberField = document.querySelector('.number-field');
-const numberControl = numberField.querySelector('.number-field__control');
 
-numberField.querySelector('.number-field__button--minus').addEventListener('click', () => {
-  numberControl.stepDown();
-});
+if (numberField) {
+  const numberControl = numberField.querySelector('.number-field__control');
 
-numberField.querySelector('.number-field__button--plus').addEventListener('click', () => {
-  numberControl.stepUp();
-});
+  numberField.querySelector('.number-field__button--minus').addEventListener('click', () => {
+    numberControl.stepDown();
+  });
+
+  numberField.querySelector('.number-field__button--plus').addEventListener('click', () => {
+    numberControl.stepUp();
+  });
+}
+
+// Thumbnails slider
+
+const thumbnailSlider = document.querySelector('.thumbnail-slider');
+
+if (thumbnailSlider) {
+  const swiper = new Swiper(".thumbnail-slider__thumbs", {
+    direction: "vertical",
+    spaceBetween: 5,
+    loop: true,
+    slidesPerView: 2.5,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      1020: {
+        spaceBetween: 10,
+        slidesPerView: 2.45,
+      },
+    },
+  });
+
+  const swiper2 = new Swiper(".thumbnail-slider__full", {
+    loop: true,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}
+
+// Like Button
+
+const likeButton = document.querySelector('.social-button--like');
+
+if (likeButton) {
+  likeButton.addEventListener('click', () => {
+    likeButton.classList.toggle('social-button--like-active');
+  });
+}
+
+// Clip Box
+
+const clipbox = document.querySelector('.clipbox');
+
+if (clipbox) {
+  clipbox.querySelector('.clipbox__toggler').addEventListener('click', () => {
+    clipbox.classList.toggle('clipbox--reduced');
+    clipbox.classList.toggle('clipbox--expanded');
+  });
+}
