a847c48cefd94763d9e8c3c551a81dbb8e423c4d
Rustam <rustam.sb@yandex.ru>
2022-07-11 15:10:10 +0300

    add new sections

--- source/js/umnyash.js
+++ source/js/umnyash.js
@@ -1,17 +1,15 @@
-// Popup
-
-const Keys = {
-  ESCAPE: 'Escape',
-  ESC: 'Esc',
-};
-
-const isEscEvent = (evt) => {
-  return evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
-};
-
 const popup = document.querySelector('.popup');
 
 if (popup) {
+  const Keys = {
+    ESCAPE: 'Escape',
+    ESC: 'Esc',
+  };
+
+  const isEscEvent = (evt) => {
+    return evt.key === Keys.ESCAPE || evt.key === Keys.ESC;
+  };
+
   const popupCloseButton = popup.querySelector('.popup__close')
 
   const openPopup = () => {
@@ -38,9 +36,6 @@ if (popup) {
   openPopup();
 }
 
-
-// Number Field
-
 const numberField = document.querySelector('.number-field');
 
 if (numberField) {
@@ -54,8 +49,6 @@ if (numberField) {
     numberControl.stepUp();
   });
 }
-
-// Thumbnails slider
 
 const thumbnailSlider = document.querySelector('.thumbnail-slider');
 
@@ -83,8 +76,6 @@ if (thumbnailSlider) {
   });
 }
 
-// Like Button
-
 const likeButton = document.querySelector('.social-button--like');
 
 if (likeButton) {
@@ -93,18 +84,20 @@ if (likeButton) {
   });
 }
 
-// Clip Box
+const clipboxes = document.querySelectorAll('.clipbox');
 
-const clipbox = document.querySelector('.clipbox');
+if (clipboxes) {
+  clipboxes.forEach((clipbox) => {
+     if (clipbox.offsetHeight > parseInt(getComputedStyle(clipbox).getPropertyValue('--max-height'), 10)) {
+        clipbox.classList.add('clipbox--reduced');
+      }
 
-if (clipbox) {
-  clipbox.querySelector('.clipbox__toggler').addEventListener('click', () => {
-    clipbox.classList.toggle('clipbox--reduced');
-    clipbox.classList.toggle('clipbox--expanded');
+      clipbox.querySelector('.clipbox__toggler').addEventListener('click', () => {
+      clipbox.classList.toggle('clipbox--reduced');
+      clipbox.classList.toggle('clipbox--expanded');
+    });
   });
 }
-
-// Works Gallery
 
 const worksGallerySlider = document.querySelector('.works-gallery__slider');
 
@@ -122,5 +115,47 @@ if (worksGallerySlider) {
         spaceBetween: 18,
       },
     },
+  });
+}
+
+const goodInfoTaber = document.querySelector('.good-info__taber');
+
+if (goodInfoTaber) {
+  const swiper = new Swiper(".good-info__tab-buttons-wrapper", {
+    slidesPerView: 'auto',
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+  const swiper2 = new Swiper(".good-info__tabs-wrapper", {
+    spaceBetween: 50,
+    effect: "fade",
+    allowTouchMove: false,
+    fadeEffect: {
+      crossFade: true
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+}
+
+const radioselect = document.querySelector('.select-2');
+
+if (radioselect) {
+  const radioselectButton = radioselect.querySelector('.select-2__button');
+
+  radioselectButton.addEventListener('click', () => {
+    radioselect.classList.toggle('select-2--open');
+  });
+
+  const radioselectOptionsWrapper = radioselect.querySelector('.select-2__options');
+  const radioselectButtonValue = radioselectButton.querySelector('.select-2__button-value');
+
+  radioselectOptionsWrapper.addEventListener('click', (evt) => {
+    if (evt.target.className === 'select-2__radiobutton-label') {
+      radioselectButtonValue.textContent = evt.target.textContent;
+      radioselect.classList.remove('select-2--open');
+    }
   });
 }
