b4736ebfc31bdae4d7f4b6373a461c2cd1c3a9ea
Rustam <rustam17.sb@gmail.com>
2022-12-14 16:58:02 +0300

    feat: add delivery-self info

--- source/js/new.js
+++ source/js/new.js
@@ -575,3 +575,16 @@ if (linksElements) {
     })
   });
 }
+
+const orderForm = document.querySelector('.custom-form-2--order');
+const formSectionBodyAddress = orderForm.querySelector('.form__section-body--address');
+
+if (orderForm && formSectionBodyAddress) {
+  orderForm.addEventListener('change', (evt) => {
+    const deliverySelfCheckbox = evt.target.closest('.form__checkbox-control');
+    if (!deliverySelfCheckbox) {
+      return;
+    }
+    formSectionBodyAddress.classList.toggle('form__section-body--address--stock', deliverySelfCheckbox.checked);
+  });
+}
