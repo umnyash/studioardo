b67a6237adf283ef4a8759635f0e839e4e61dfc7
Rustam <rustam17.sb@gmail.com>
2022-12-31 01:59:31 +0300

    refactor: new build

--- gulpfile.js
+++ gulpfile.js
@@ -1,259 +1,137 @@
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
+// Type
+const isProd = process.argv.includes('--production');
+const isDev = !isProd;
+
+// Plugins
+const {src, dest, watch, series, parallel} = require('gulp');
 const del = require('del');
-const htmlmin = require('gulp-htmlmin');
-const rename = require('gulp-rename');
+const sync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('autoprefixer');
-const postcss = require('gulp-postcss');
-const csso = require('postcss-csso');
-const sourcemap = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const groupCssMediaQueries = require('gulp-group-css-media-queries');
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const terser = require('gulp-terser');
+const gulpIf = require('gulp-if');
+const newer = require('gulp-newer');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
-const svgstore = require('gulp-svgstore');
-const terser = require('gulp-terser');
-const sync = require('browser-sync').create();
-
-// HTML
-
-const html = () => {
-  return gulp.src('source/**/*.html')
-    // .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
-}
-
-// Styles
-
-const styles = () => {
-  return gulp.src('source/sass/style.scss')
-    // .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer(),
-      // csso()
-    ]))
-    .pipe(rename('new.css'))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
-    .pipe(sync.stream());
-}
-
-exports.styles = styles;
-
-// Styles2
-
-const styles2 = () => {
-  return gulp.src('source/sass/style.scss')
-    // .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer(),
-      csso()
-    ]))
-    .pipe(rename('new.min.css'))
-    .pipe(sourcemap.write('.'))
-    .pipe(gulp.dest('build/css'))
-    .pipe(sync.stream());
-}
-
-exports.styles2 = styles2;
-
-// Images
-
-const optimizeImages = () => {
-  return gulp.src([
-    'source/img/**/*.{png,jpg,svg}',
-    'source/files/**/*.{png,jpg,svg}'
-  ], {
-    base: 'source'
-  })
-    .pipe(imagemin([
-      imagemin.mozjpeg({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.svgo({
-        plugins: [
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
-    .pipe(gulp.dest('build'))
-}
-
-exports.images = optimizeImages;
-
-const copyImages = () => {
-  return gulp.src([
-    'source/img/**/*.{png,jpg,svg}',
-    'source/files/**/*.{png,jpg,svg}'
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'));
-}
-
-exports.images = copyImages;
-
-// WebP
-
-// const createWebp = () => {
-//   return gulp.src([
-//     'source/img/**/*.{jpg,png}',
-//     '!source/img/backgrounds/**',
-//     '!source/img/favicons/**',
-//     '!source/img/location-pin.png',
-//     'source/files/**/*.{jpg,png}'
-//   ], {
-//     base: 'source'
-//   })
-//     .pipe(webp({quality: 90}))
-//     .pipe(gulp.dest('build'));
-// }
-
-const createWebp = () => {
-  return gulp.src([
-    'source/img/stone/hero/*.{jpg,png}',
-    'source/img/team/*.{jpg,png}',
-    'source/img/offers/*.{jpg,png}'
-  ], {
-    base: 'source'
-  })
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest('build'));
-}
-
-exports.createWebp = createWebp;
-
-// Sprite
-
-const sprite = () => {
-  return gulp.src('source/img/copyright-logo.svg')
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
-}
-
-exports.sprite = sprite;
 
 // Copy
-
 const copy = () => {
-  return gulp.src([
-    'source/fonts/*.{woff2,woff}',
-    'source/*.ico',
-    'source/*.webmanifest',
-    'source/css/*.css',
-    '!source/css/umnyash.css',
-    'source/js/**/*.js',
-    '!source/js/umnyash.js',
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'))
-  done();
-}
+  return src([
+    './source/fonts/*.*',
+    './source/css/*.css',
+    './source/js/**/*.js',
+    '!./source/js/new.js',
+    '!./source/js/demo.js'
+  ], { base: './source'})
+    .pipe(dest('./build'));
+};
 
-exports.copy = copy;
+// HTML
+const html = () => {
+  return src('./source/*.html')
+    .pipe(dest('./build'))
+    .pipe(sync.stream())
+};
+
+// Styles
+const styles = () => {
+  return src('./source/sass/new.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulpIf(isProd, groupCssMediaQueries()))
+    .pipe(dest('./build/css', { sourcemaps: true }))
+    // .pipe(rename({ suffix: '.min' }))
+    // .pipe(csso())
+    // .pipe(dest('./build/css', { sourcemaps: true }))
+    .pipe(sync.stream())
+};
 
 // Scripts
-
 const scripts = () => {
-  return gulp.src('source/js/new.js')
+  return src(['./source/js/new.js', './source/js/demo.js'], { sourcemaps: true })
     // .pipe(terser())
-    // .pipe(rename('script.min.js'))
-    .pipe(gulp.dest('build/js'))
-    .pipe(sync.stream());
+    .pipe(dest('./build/js', { sourcemaps: true }))
+    .pipe(sync.stream())
+};
+
+// Images
+const images = () => {
+  return src('./source/img/**/*.{png,jpg,jpeg,svg}')
+  .pipe(newer('./build/img'))
+  .pipe(gulpIf(isProd, imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo({
+      plugins: [
+        { cleanupIDs: false }
+      ]
+    })
+  ])))
+  .pipe(dest('./build/img'))
+};
+
+// Webp
+const createWebp = () => {
+  return src([
+    './source/img/stone/hero/*.{jpg,jpeg,png}',
+    './source/img/team/*.{jpg,jpeg,png}',
+    './source/img/offers/*.{jpg,jpeg,png}'
+  ], { base: './source' })
+    .pipe(webp({quality: 90}))
+    .pipe(dest('./build'));
 }
 
-exports.scripts = scripts;
+// Cleaning
+const clear = () => {
+  return del('./build');
+};
 
-// Scripts2
-
-const scripts2 = () => {
-  return gulp.src('source/js/new.js')
-    .pipe(terser())
-    .pipe(rename('new.min.js'))
-    .pipe(gulp.dest('build/js'))
-    .pipe(sync.stream());
-}
-
-exports.scripts2 = scripts2;
-
-// Clean
-
-const clean = () => {
-  return del('build');
+// Watching
+const watcher = () => {
+  watch('./source/*.html', html);
+  watch('./source/sass/**/*.scss', styles);
+  watch(['./source/js/new.js', './source/js/demo.js'], scripts);
+  watch('./source/img/**/*.{png,jpg,jpeg,svg}', images);
+  watch([
+    './source/img/stone/hero/*.{jpg,jpeg,png}',
+    './source/img/team/*.{jpg,jpeg,png}',
+    './source/img/offers/*.{jpg,jpeg,png}'
+  ], createWebp);
 }
 
 // Server
-
-const server = (done) => {
+const server = () => {
   sync.init({
     server: {
-      baseDir: 'build'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
+      baseDir: './build'
+    }
   });
-  done();
-}
+};
 
-exports.server = server;
+// Tasks
+exports.copy = copy;
+exports.html = html;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.createWebp = createWebp;
+exports.watch = watcher;
+exports.clear = clear;
 
-// Reload
-
-const reload = () => {
-  sync.reload();
-  done();
-}
-
-// Watcher
-
-const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles2));
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/js/script.js', gulp.series(scripts));
-  gulp.watch('source/*.html', gulp.series(html, reload));
-}
 
 // Build
-
-const build = gulp.series(
-  clean,
-  copy,
-  optimizeImages,
-  gulp.parallel(
-    styles,
-    styles2,
-    html,
-    scripts,
-    scripts2,
-    createWebp
-    // sprite,
-  ),
+const build = series(
+  clear,
+  parallel(copy, html, styles, scripts, images, createWebp),
 );
 
-exports.build = build;
-
-// Default
-
-exports.default = gulp.series(
-  clean,
-  copy,
-  copyImages,
-  gulp.parallel(
-    styles,
-    styles2,
-    html,
-    scripts,
-    scripts2,
-    createWebp
-    // sprite,
-  ),
-  server,
-  watcher
+// Dev
+const dev = series(
+  build,
+  parallel(server, watcher)
 );
+
+exports.default = isProd ? build : dev;
